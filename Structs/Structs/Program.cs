9550009862a044494c81b0b69bcc25bead07518d
Ibrohim avazov <ibrohim.avazov.98@mail.ru>
2019-10-14 14:09:44 +0500

    add struct

--- Structs/Structs/Program.cs
+++ Structs/Structs/Program.cs
@@ -2,11 +2,33 @@
 
 namespace Structs
 {
+    struct User {
+       // public string name = "Sam";      ! Ошибка
+       // public int age = 23;             ! Ошибка
+        public string name;
+        public int age;
+        public void Info() {
+            Console.WriteLine($"Ism: {name} Yosh:  {age}");
+        }
+        public User(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Как и класс, структура может определять констукторы. Но в отличие от класса нам не обязательно вызывать конструктор для создания объекта структуры
+            User tom;
+            tom.name = "Tom";
+            tom.age = 12;
+            tom.Info();
+            //инициализатор для создания структуры
+            User bob = new User("Bob", 34);
+            bob.Info();
+
         }
     }
 }
