f781e1eb1cd0c467e27f8e5c795ad6e6430530c6
CSharpLesson <56337975+CSharpLesson@users.noreply.github.com>
2019-11-07 13:14:02 +0500

    Update Polimorfizm.cs

--- Polimorfizm.cs
+++ Polimorfizm.cs
@@ -25,3 +25,92 @@ class Dog : Animal  // Derived class (child)
     Console.WriteLine("The dog says: bow wow");
   }
 }
+
+
+
+
+_____________________________________________________________________________________________________________________________________________________________________________
+class Animal  // Base class (parent) 
+{
+  public void animalSound() 
+  {
+    Console.WriteLine("The animal makes a sound");
+  }
+}
+
+class Pig : Animal  // Derived class (child) 
+{
+  public void animalSound() 
+  {
+    Console.WriteLine("The pig says: wee wee");
+  }
+}
+
+class Dog : Animal  // Derived class (child) 
+{
+  public void animalSound() 
+  {
+    Console.WriteLine("The dog says: bow wow");
+  }
+}
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+    Animal myAnimal = new Animal();  // Create a Animal object
+    Animal myPig = new Pig();  // Create a Pig object
+    Animal myDog = new Dog();  // Create a Dog object
+
+    myAnimal.animalSound();
+    myPig.animalSound();
+    myDog.animalSound();
+  }
+}
+//The animal makes a sound
+//The animal makes a sound
+//The animal makes a sound
+Не тот результат, который я искал
+Вывод из приведенного выше примера, вероятно, не соответствует ожиданиям. Это потому, что метод базового класса переопределяет метод производного класса, когда они имеют одно и то же имя.
+
+Однако C # предоставляет возможность переопределить метод базового класса, добавив virtualключевое слово к методу внутри базового класса и используя overrideключевое слово для каждого метода производного класса:
+
+
+class Animal  // Base class (parent) 
+{
+  public virtual void animalSound() 
+  {
+    Console.WriteLine("The animal makes a sound");
+  }
+}
+
+class Pig : Animal  // Derived class (child) 
+{
+  public override void animalSound() 
+  {
+    Console.WriteLine("The pig says: wee wee");
+  }
+}
+
+class Dog : Animal  // Derived class (child) 
+{
+  public override void animalSound() 
+  {
+    Console.WriteLine("The dog says: bow wow");
+  }
+}
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+    Animal myAnimal = new Animal();  // Create a Animal object
+    Animal myPig = new Pig();  // Create a Pig object
+    Animal myDog = new Dog();  // Create a Dog object
+
+    myAnimal.animalSound();
+    myPig.animalSound();
+    myDog.animalSound();
+  }
+}
+
