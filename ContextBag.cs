83e2cf406c8dcecd485e60d003a32d00ed8e3315
Ibrohim <56337975+CSharpLesson@users.noreply.github.com>
2020-11-20 18:29:28 +0500

    Update ContextBag.cs

--- ContextBag.cs
+++ ContextBag.cs
@@ -1,4 +1,4 @@
-xato
+//xato
 var documentTabs = (await _productTabDocumentService.FindAsync(s => s.DocumentId == documentId)).ToList();
 
                 
@@ -13,4 +13,23 @@ var documentTabs = (await _productTabDocumentService.FindAsync(s => s.DocumentId
                         }
                         _productService.UpdateLastPrice(tab.ProductId, tab.Price, tab.SellPrice);
                         _productBalanceService.UpdatePrice(CompanyId, document.WarehouseId, tab);
+                    }
+
+//to'g'ri
+var documentTabs = (await _productTabDocumentService.FindAsync(s => s.DocumentId == documentId)).ToList();
+
+                
+                    foreach (var tab in documentTabs)
+                    {
+                        var lastPrice = _productService.GetLastPrice(CompanyId, document.WarehouseId, tab.ProductId, tab.Barcode);// hech qachon bazadan malumot olib unga o'zgartirish qilib va boshqa boshqa tableni update yoki add yoki delete qilib bo'midi unda ozgatririlgan tablitsani ha saxranit qiladi bunaqa bo'masligi uchun uni huddi shuni yengi modeliga olib qilish kere
+                         var doctype = new ProductTabDocument();
+                         doctype = tab;
+                        if (lastPrice != null)
+                        {
+                            doctype.Price = lastPrice.IncomePrice;
+                            if (!confirm) //confirm otmenit qilinganda sotilish narx oldingi dkumentdagi narx belgilanadi
+                            doctype.SellPrice = lastPrice.SellPrice;
+                        }
+                        _productService.UpdateLastPrice(doctype.ProductId, doctype.Price, doctype.SellPrice);
+                        _productBalanceService.UpdatePrice(CompanyId, document.WarehouseId, doctype);
                     }
